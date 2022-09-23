49aa0fb19072950540541378aefcc847b59babf7
Ricardo Pontes <00119110450@pq.uenf.br>
2022-09-23 16:44:12 -0300

    Update

---
+++ racket/Laboratorio Racket 2022/exercicio-slide 1.rkt
@@ -0,0 +1,23 @@
+;; Introdução à Linguagem Scheme-Racket
+;; Prof. Ausberto S. Castro Vera       (ascv@uenf.br)
+;; UENF-CCT-LCMAT - Curso de Ciencia da Computacao
+;; Setembro - 2022 
+;; Aluno: Ricardo Willian Pontes da Silva
+;;
+#lang racket      ;; define a linguagem default
+; ------------------------------------------------
+(newline)
+(display "  UENF-CCT-LCMAT-CC, 2022")
+(newline)
+(display "  Paradigmas de Linguagens de Programação (Prof. Ausberto Castro)")
+(newline)
+(display "  Aluno: Ricardo Willian Pontes da Silva ")
+(newline)
+;;
+
+(define fun
+      (lambda  (x  y)
+       (-(/( + 2 x)(- (* 3 y) 4))(+(+ x y)5))
+       )
+) 
+(fun 2 1)
