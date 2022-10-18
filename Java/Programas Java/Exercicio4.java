cd397b0cd544bf89e6435e6adeb9c90651557ff9
Ricardo Pontes <00119110450@pq.uenf.br>
2022-10-18 09:17:43 -0300

    Update

---
+++ Java/Programas Java/Exercicio4.java
@@ -0,0 +1,21 @@
+package exercicioPLP;
+import java.util.Scanner;
+
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+	System.out.print("Digite o primeiro número inteiro: ");
+	int numero1 = input.nextInt();
+	
+	System.out.print("Digite o segundo número inteiro: ");
+	int numero2 = input.nextInt();
+	
+	System.out.printf("\nA soma entre %d com %d é de: %d",numero1,numero2,numero1+numero2);
+	System.out.printf("\nA subtração entre %d com %d é de: %d",numero1,numero2,numero1-numero2);
+	System.out.printf("\nA multiplicação entre %d com %d é de: %d",numero1,numero2,numero1*numero2);
+	System.out.printf("\nA divisão entre %d com %d é de: %d",numero1,numero2,numero1/numero2);
+	}
+
+}
