eb20f2b0552ec44944209f08004ccdaea4474a61
Ricardo Pontes <00119110450@pq.uenf.br>
2022-10-18 16:22:14 -0300

    Update

---
+++ Java/Programas Java/Exercicio5.java
@@ -0,0 +1,31 @@
+package exercicioPLP;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double[] notas = new double[4];
+		double media = 0;
+
+		for (int i = 0; i < notas.length; i++) {
+			System.out.printf("Digite o valor para a %d° nota: ", i + 1);
+			notas[i] = input.nextDouble();
+		}
+
+		for (int i = 0; i < notas.length; i++) {
+			 media += notas[i];
+		}
+		
+		media = media/notas.length;
+		System.out.printf("A média foi de: %.2f\n",media);
+		if (media >= 6) {
+			System.out.println("\nAprovado.");
+		} else {
+			System.out.println("\nReprovado.");
+		}
+
+	}
+
+}
